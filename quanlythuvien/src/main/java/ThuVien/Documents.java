289e891a378adbc9792c7060a7da8b688fde19de
test <nguyenthanhhung.dev@gmail.com>
2022-11-27 17:28:08 +0700

    Add add and remove

--- quanlythuvien/src/main/java/ThuVien/Documents.java
+++ quanlythuvien/src/main/java/ThuVien/Documents.java
@@ -1,34 +1,115 @@
 package ThuVien;
 
 import Polyfill.PFArray;
+import Polyfill.ThoiGian;
+
+import java.util.stream.IntStream;
+
+import static ThuVien.DangNhap.scanner;
 
 public class Documents extends Management<Document> {
     public Documents() {
     }
 
+
+    private Document acccessInpDoc() {
+        Document document = null;
+        System.out.println("Nhap ten sach: ");
+        document.setName(scanner.nextLine());
+
+        System.out.println("Nhap so luong tac gia: ");
+        Author[] authors = new Author[Integer.parseInt(scanner.nextLine())];
+        document.setAuthors(authors);
+
+        System.out.println("Nhap so luong ban sao: ");
+        document.setCopies(Integer.parseInt(scanner.nextLine()));
+
+        System.out.println("Nhap thoi gian xuat ban: ");
+        document.setPublication(ThoiGian.parseTG(scanner.nextLine()));
+
+        instance.push_back(document);
+
+        return document;
+    }
+
+    public int menuEdit() {
+        System.out.println("1. Ten  sach");
+        System.out.println("2. Tac gia");
+        System.out.println("3. Nam xuat ban");
+        System.out.println("4. So luong ban sao");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public Author accessInpAuthor() {
+        System.out.println("Nhap ma tac gia: ");
+        Author author = new Author(Integer.parseInt(scanner.nextLine()));
+        System.out.println("Nhap ten tac gia: ");
+        author.setName(scanner.nextLine());
+        System.out.println("Nhap ngo ngu: ");
+        author.setLanguage(Languages.parseLang(scanner.nextLine()));
+        System.out.println("Nhap web: ");
+        author.setWebsite(scanner.nextLine());
+        System.out.println("Nhap ngay sinh: ");
+        author.setBirth(ThoiGian.parseTG(scanner.nextLine()));
+        author.setAddress(scanner.nextLine());
+        System.out.println("Nhap email: ");
+        author.setEmail(scanner.nextLine());
+        System.out.println("Nhap so dien thoai: ");
+        author.setPhone(scanner.nextLine());
+        return author;
+    }
+
+
     @Override
     public Document add() {
-        // TODO Auto-generated method stub
-        return null;
+        Document document = acccessInpDoc();
+        instance.push_back(document);
+        return acccessInpDoc();
     }
 
     @Override
     public Document remove() {
-        // TODO Auto-generated method stub
-        return null;
+        Document document = acccessInpDoc();
+        int index = search(document.getId());
+        if (index != -1) {
+            instance.erase(index);
+        }
+
+        return document;
     }
 
     @Override
     public Document edit() {
-        // TODO Auto-generated method stub
-        return null;
+        Document document = acccessInpDoc();
+        int index = search(document.getId());
+        if (index != -1) {
+
+            document = instance.at(index);
+
+            switch (menuEdit()) {
+                case 1 -> document.setName(scanner.nextLine());
+                case 2 -> {
+                    System.out.println("Nhap so luong tac gia: ");
+                    Author[] authors = new Author[Integer.parseInt(scanner.nextLine())];
+                    IntStream.range(0, authors.length).forEach(i -> {
+                        authors[i] = accessInpAuthor();
+                    });
+
+                    document.setAuthors(authors);
+                }
+                case 3 -> document.setPublication(ThoiGian.parseTG(scanner.nextLine()));
+                case 4 -> document.setCopies(Integer.parseInt(scanner.nextLine()));
+            }
+        }
+
+        return document;
     }
 
     @Override
     public int[] search() {
-        // TODO Auto-generated method stub
-        return null;
+        return new int[0];
     }
+
 
     private PFArray<Document> instance;
 
